096758d44aa434c63df745c7cf59e16ec20ed3d8
Jonathan King <jok120@pitt.edu>
2023-05-15 16:00:09 -0400

    Monitor val loss for checkpointing.

--- train_openfold.py
+++ train_openfold.py
@@ -684,7 +684,7 @@ def main(args):
             every_n_epochs=1,
             auto_insert_metric_name=False,
             save_top_k=3,
-            monitor="train/loss",
+            monitor="val/lddt_aa",
             # dirpath=wdb_logger.experiment.dir if args.wandb else None,
             verbose=True
         )
