d9d9092318b32705b3c6d830738a51d58c437084
Jonathan King <jonathanking@berkeley.edu>
2023-09-18 00:15:20 -0400

    Update config to support angle conv_encoder.

--- openfold/config.py
+++ openfold/config.py
@@ -577,6 +577,7 @@ config = mlc.ConfigDict(
                 "epsilon": eps,  # 1e-12,
                 "inf": 1e5,
                 "use_angle_transformer": False,
+                "angle_transformer_conv_encoder": False,
                 "angle_transformer_dropout": 0.1,
                 "angle_transformer_dff": 2048,
                 "angle_transformer_heads": 4,
