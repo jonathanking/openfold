0db7c7f2750dd9bea748e31ed12157c4dbf8034f
Jonathan King <jok120@pitt.edu>
2023-09-22 20:52:41 -0400

    More research updates. Figs better organized.

--- jk_research/evaluations/aim3c/run_all_evals.sh
+++ jk_research/evaluations/aim3c/run_all_evals.sh
@@ -1,5 +1,8 @@
 # Use bash to run all jobs in jk_research/evaluations/aim3c/slurm/*.slurm that don't start with 8-
 
+echo "WARNING!! NOT RUNNING PART 8!!"
+exit 0
+
 for file in jk_research/evaluations/aim3c/slurm/*.slurm; do
     if [[ $file != *"8-AT-noOMM-whole"* ]]; then
         echo "Running $file"
