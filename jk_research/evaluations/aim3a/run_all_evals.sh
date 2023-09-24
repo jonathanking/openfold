673d4abb8aad375464c76d4ee81002bab1a171fa
Jonathan King <jok120@pitt.edu>
2023-09-24 14:40:58 -0400

    Research updates.

--- jk_research/evaluations/aim3a/run_all_evals.sh
+++ jk_research/evaluations/aim3a/run_all_evals.sh
@@ -1,11 +1,17 @@
 echo "MAKE SURE YOU UPDATE THE CHECKPOINT PATHS IN THE CORRESPONDING SCRIPTS FIRST!!"
-exit 0
+# exit 0
 
 # Baseline 1
+echo "Running jk_research/evaluations/aim3a/slurm/resnet_baseline.slurm"
 bash jk_research/evaluations/aim3a/slurm/resnet_baseline.slurm
 # Baseline 2
+echo "Running jk_research/evaluations/aim3a/slurm/resnet_baseline_not_AF2_toastyRN1.slurm"
 bash jk_research/evaluations/aim3a/slurm/resnet_baseline_not_AF2_toastyRN1.slurm
+
 # Maybe the best non-conv model
+echo "Running jk_research/evaluations/aim3a/slurm/best-toastyC0_repeat_noconvB.slurm"
 bash jk_research/evaluations/aim3a/slurm/best-toastyC0_repeat_noconvB.slurm
+
 # A good conv model
+echo "Running jk_research/evaluations/aim3a/slurm/young_sweep_159.slurm"
 bash jk_research/evaluations/aim3a/slurm/young_sweep_159.slurm
